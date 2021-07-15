769fc35fe1b0ebb4de138db3b671d6d962628d02
Mohamed Masalati <mohamed.masalati@ondewo.com>
2021-07-15 12:33:31 +0200

    updated export so it deletes duplicates before backup

--- examples/export/export.py
+++ examples/export/export.py
@@ -1,7 +1,9 @@
 import argparse
+import json
 import os
+import shutil
 import zipfile
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Union
 
 import polling
 from google.longrunning.operations_pb2 import Operation, GetOperationRequest
@@ -11,7 +13,7 @@ from ondewo.nlu.agent_pb2 import ExportAgentRequest, ExportAgentResponse
 from ondewo.nlu.agent_pb2 import ListAgentsRequest, AgentView, ListAgentsResponse
 from ondewo.nlu.client import Client
 from ondewo.nlu.client_config import ClientConfig
-import json
+
 
 def export_agents(args: argparse.Namespace) -> None:
     # Set the client configs and create a client instance
@@ -21,7 +23,12 @@ def export_agents(args: argparse.Namespace) -> None:
     # example would be [("projects/2fa32d44-a654-4772-bebb-87fef894d0e5/agent,"ATC-startup project"),  ... ]
     agents_info: List[Tuple[str, str]] = get_agents_info(client=client)
 
-    dir_path = create_path_if_not_exists(args)
+    exported_agents_dir_name: str = args.exported_agents_dir_name
+    exported_agents_dir_name = create_path_if_not_exists(exported_agents_dir_name)
+
+    logger.info("Start cleaning old data")
+    clean_old_version(agents_info, exported_agents_dir_name)
+    logger.info("End cleaning old data")
 
     logger.info("Start exporting all agents \n ========================")
 
@@ -52,23 +59,17 @@ def export_agents(args: argparse.Namespace) -> None:
         export_response: ExportAgentResponse = ExportAgentResponse()
         export_operation_update.response.Unpack(export_response)
         if export_response.agent_content:
-            with open(f'{dir_path}/{display_name}.zip', mode='wb') as zf:
+            with open(f'{exported_agents_dir_name}/{display_name}.zip', mode='wb') as zf:
                 zf.write(export_response.agent_content)
-            zip_extract(dir_path)
-            create_change_log_md(dir_path)
-            create_project_name_md(dir_path, display_name, project_id)
+            zip_extract(exported_agents_dir_name)
+            create_change_log_md(exported_agents_dir_name)
+            create_project_name_md(exported_agents_dir_name, display_name, project_id)
             logger.info(f"End exporting {display_name} project")
         else:
             logger.warning(f"Failed to export {display_name} project")
 
     logger.info("======================== \n End exporting all agents")
 
-def create_path_if_not_exists(args):
-    exported_agents_dir_name: str = args.exported_agents_dir_name
-    dir_path: str = os.path.join(os.getcwd(), exported_agents_dir_name)
-    if not os.path.exists(dir_path):
-        os.mkdir(dir_path)
-    return dir_path
 
 def get_agents_info(client: Client) -> List[Tuple[str, str]]:
     # This will contain List containing tuples "which are the info of the agent" of each agent
@@ -86,6 +87,24 @@ def get_agents_info(client: Client) -> List[Tuple[str, str]]:
     # returns the parent and display name of all agents
     return agents_info
 
+
+def create_path_if_not_exists(exported_agents_dir_name: str) -> str:
+    dir_path: str = os.path.join(os.getcwd(), exported_agents_dir_name)
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    return dir_path
+
+
+def clean_old_version(agents_info: List[Tuple[str, str]], exported_agents_dir_name: str) -> None:
+    projects_directories: List[str] = get_all_directories(exported_agents_dir_name)
+    project_name_to_be_updated: List[str] = [x[1] for x in agents_info]
+
+    for project in projects_directories:
+        if project in project_name_to_be_updated:
+            full_path: str = os.path.join(exported_agents_dir_name, project)
+            shutil.rmtree(full_path)
+
+
 def zip_extract(directory: str) -> None:
     extension: str = ".zip"
     os.chdir(directory)
@@ -99,41 +118,44 @@ def zip_extract(directory: str) -> None:
             os.remove(zip_name)  # delete zipped file
 
 
-
-def create_change_log_md(dir_path):
-    projects_directories = get_all_directories(dir_path)
+def create_change_log_md(dir_path: str) -> None:
+    projects_directories: List[str] = get_all_directories(dir_path)
     for dir in projects_directories:
-        base_path = os.path.abspath(dir)
-        config_data = get_data_json(base_path)
-        change_log_md = os.path.join(base_path,"CHANGELOG.MD")
+        base_path: str = os.path.abspath(dir)
+        config_data: Dict = get_data_json(base_path)
+        change_log_md: str = os.path.join(base_path, "CHANGELOG.MD")
         with open(change_log_md, 'w') as f:
-            description = config_data["agent_config"]["description"]
+            description: Union[str, List[str]] = config_data["agent_config"]["description"]
             for d in description:
                 f.write(d)
 
-def create_project_name_md(dir_path,display_name,project_id):
-    projects_directories = get_all_directories(dir_path)
+
+def create_project_name_md(dir_path, display_name, project_id) -> None:
+    projects_directories: List[str] = get_all_directories(dir_path)
     for dir in projects_directories:
         base_path = os.path.abspath(dir)
         project_name_md = os.path.join(base_path, "PROJECTNAME.MD")
         with open(project_name_md, 'w+') as f:
-            f.write(display_name+'\n')
+            f.write(display_name + '\n')
             f.write(project_id)
 
-def get_all_directories(dir_path):
+
+def get_all_directories(dir_path: str) -> List[str]:
     projects_directories_iter: Tuple[str, List[str], List[str]] = next(os.walk(dir_path), ('', [], []))
     projects_directories: List[str] = projects_directories_iter[1]
     return projects_directories
 
-def get_data_json(dir_path):
-    config_path = os.path.join(dir_path, "OndewoConfig.json")
+
+def get_data_json(dir_path: str) -> Dict:
+    config_path: str = os.path.join(dir_path, "OndewoConfig.json")
     with open(config_path) as f:
-        config_data = json.load(f)
+        config_data: Dict = json.load(f)
     return config_data
+
 
 def arg_parse() -> argparse.Namespace:
     description: str = "Export example, so usage would be like: \n\n" \
-                       "\t\tpython -m export --config=\"$(cat conf.json)\" --exported-agents-dir-name=2.6.x --secure"
+                       "\t\tpython -m export --config=\"$(cat config.json)\" --exported-agents-dir-name=2.6.x --secure"
     parser = argparse.ArgumentParser(
         description=description,
         formatter_class=argparse.RawTextHelpFormatter
@@ -144,6 +166,8 @@ def arg_parse() -> argparse.Namespace:
     args: argparse.Namespace = parser.parse_args()
     return args
 
+
 if __name__ == '__main__':
     args = arg_parse()
+    # Start export
     export_agents(args=args)
