9f2f09414827cb0385f8821ee7b89037f8d0e7de
atis06 <32523004+atis06@users.noreply.github.com>
2022-02-04 11:49:31 +0100

    Release/2.4.0 (#43)
    ## Release ONDEWO NLU Python Client 2.4.0
    
    ### New features
    * [OND211-1836] Support intent tags with filtering
    * [OND211-1850] API Update. Allow sorting and searching of entity values on listing
    * [OND211-1850] API Update. Enable Entity Type View on all Entity Type CRUD operations
    
    ### Improvements
    * [OND211-1911] Updated ondewo-logging version
    * Updated requirements compatibility
    
    ### Bug fixes
    * [OND211-1911] Update Ondewo Logging dependency to fix the Log4J vulnerability on ElasticSearch

--- ondewo/nlu/services/intents.py
+++ ondewo/nlu/services/intents.py
@@ -26,7 +26,7 @@ from ondewo.nlu.intent_pb2 import BatchCreateTrainingPhrasesRequest, BatchTraini
     BatchDeleteResponseMessagesRequest, ListResponseMessagesResponse, ListResponseMessagesRequest, \
     BatchParametersStatusResponse, BatchCreateParametersRequest, BatchGetParametersRequest, \
     BatchUpdateParametersRequest, BatchDeleteParametersResponse, BatchDeleteParametersRequest, \
-    ListParametersResponse, ListParametersRequest
+    ListParametersResponse, ListParametersRequest, IntentTagMessage
 from ondewo.nlu.intent_pb2_grpc import IntentsStub
 
 
@@ -162,4 +162,12 @@ class Intents(ServicesInterface):
 
     def list_parameters(self, request: ListParametersRequest) -> ListParametersResponse:
         response: ListParametersResponse = self.stub.ListParameters(request, metadata=self.metadata)
+        return response
+
+    def tag_intent(self, request: IntentTagMessage) -> empty_pb2.Empty:
+        response: empty_pb2.Empty = self.stub.TagIntent(request, metadata=self.metadata)
+        return response
+
+    def delete_intent_tag(self, request: IntentTagMessage) -> empty_pb2.Empty:
+        response: empty_pb2.Empty = self.stub.DeleteIntentTag(request, metadata=self.metadata)
         return response
