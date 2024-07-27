2a3741871907df9139725eb53c172a2f958e0ac2
Andreas Rath <andreas.rath@ondewo.com>
2024-07-27 10:45:18 +0200

    Correct setup format

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     version='5.0.1',
     author='ONDEWO GmbH',
     author_email='office@ondewo.com',
-    description='This library facilitates the interaction between users and ONDEWO NLU servers.',
+    description='ONDEWO Natural Language Understanding (NLU) Client library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ondewo/ondewo-nlu-client-python',
