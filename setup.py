29e8a61bed27c9f4a10645989c7c3646e930755b
Andreas Rath <andreas.rath@ondewo.com>
2024-07-27 07:44:04 +0200

    Updated description on setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     version='5.0.1',
     author='Ondewo GmbH',
     author_email='office@ondewo.com',
-    description='This library facilitates the interaction between a user and his/her CAI server.',
+    description='This library facilitates the interaction between users and ONDEWO NLU servers.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ondewo/ondewo-nlu-client-python',
